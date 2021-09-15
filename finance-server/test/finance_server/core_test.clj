5f6f41322373812a070c14f64034ce359c8a9d23
Сергей Тайлаков <s.tailakov@gmail.com>
2021-09-15 20:06:56 +0200

    Init

---
+++ finance-server/test/finance_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns finance-server.core-test
+  (:require [clojure.test :refer :all]
+            [finance-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
